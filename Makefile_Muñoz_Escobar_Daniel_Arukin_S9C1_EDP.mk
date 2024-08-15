4d3260ce3cc98f36fe41376457a8708ebd76b5c2
DanielArukin <162343794+DanielArukin@users.noreply.github.com>
2024-08-15 16:19:19 -0500

    Add files via upload

---
+++ Makefile_Muñoz_Escobar_Daniel_Arukin_S9C1_EDP.mk
@@ -0,0 +1,20 @@
+resultados.pdf : resultados.tex plots_muñoz_escobar_daniel_arukin_s9c1_edp.pdf
+
+plots_muñoz_escobar_daniel_arukin_s9c1_edp.pdf:	plots_muñoz_escobar_daniel_arukin_s9c1_edp.py Placa_de_calor,_t=0.dat Placa_de_calor,_t=100.dat Placa_de_calor,_t=1000.dat Placa_de_calor,_t=2500.data
+	python3 plots_muñoz_escobar_daniel_arukin_s9c1_edp.py
+
+Placa_de_calor,_t=0.dat : Muñoz_Escobar_Daniel_Arukin_S9C1_EDP.cpp
+	g++ Muñoz_Escobar_Daniel_Arukin_S9C1_EDP
+	./a.out>>Placa_de_calor,_t=0.dat
+
+Placa_de_calor,_t=100.dat : Muñoz_Escobar_Daniel_Arukin_S9C1_EDP.cpp
+	g++ Muñoz_Escobar_Daniel_Arukin_S9C1_EDP
+	./a.out>>Placa_de_calor,_t=100.dat
+
+Placa_de_calor,_t=1000.dat : Muñoz_Escobar_Daniel_Arukin_S9C1_EDP.cpp
+	g++ Muñoz_Escobar_Daniel_Arukin_S9C1_EDP
+	./a.out>>Placa_de_calor,_t=1000.dat
+
+Placa_de_calor,_t=2500.dat : Muñoz_Escobar_Daniel_Arukin_S9C1_EDP.cpp
+	g++ Muñoz_Escobar_Daniel_Arukin_S9C1_EDP
+	./a.out>>Placa_de_calor,_t=2500.dat
