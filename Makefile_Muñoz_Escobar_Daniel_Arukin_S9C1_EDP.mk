7c5c119a9f0b151891261e3f9c0b604c5cc12855
DanielArukin <162343794+DanielArukin@users.noreply.github.com>
2024-08-15 15:59:06 -0500

    Update Makefile_Muñoz_Escobar_Daniel_Arukin_S9C1_EDP.mk

--- Makefile_Muñoz_Escobar_Daniel_Arukin_S9C1_EDP.mk
+++ Makefile_Muñoz_Escobar_Daniel_Arukin_S9C1_EDP.mk
@@ -1,5 +1,5 @@
-PLOTS_Muñoz_Escobar_Daniel_Arukin_S9C1_EDP.pdf:	PLOTS_Muñoz_Escobar_Daniel_Arukin_S9C1_EDP.py Placa_de_calor,_t=0.dat Placa_de_calor,_t=100.dat Placa_de_calor,_t=1000.dat Placa_de_calor,_t=2500.data
-	python3 PLOTS_Muñoz_Escobar_Daniel_Arukin_S9C1_EDP.py
+PLOTS_Muñoz_Escobar_Daniel_Arukin_S9C1_EDP.pdf:	plots_muñoz_escobar_daniel_arukin_s9c1_edp.py Placa_de_calor,_t=0.dat Placa_de_calor,_t=100.dat Placa_de_calor,_t=1000.dat Placa_de_calor,_t=2500.data
+	python3 plots_muñoz_escobar_daniel_arukin_s9c1_edp.py
 
 Placa_de_calor,_t=0.dat:	Muñoz_Escobar_Daniel_Arukin_S9C1_EDP.cpp
 	g++ Muñoz_Escobar_Daniel_Arukin_S9C1_EDP
